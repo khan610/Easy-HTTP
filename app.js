b32b3fa6222fb239da3444ad626eaa8d6f80ff1f
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-03 04:06:40 +0200

    Update

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = new easyHTTP();
+
+// Get Posts
+const posts = http.get(
+  'https://jsonplaceholder.typicode.com/posts',
+  function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(posts);
+    }
+  }
+);
+
+console.log(posts);
